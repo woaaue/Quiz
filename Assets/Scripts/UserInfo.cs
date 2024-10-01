0da3b5b0a578eeb49992c50f0625b176200605c3
woaaue <safat200@mail.ru>
2024-10-01 15:23:26 +0300

    create user info(intermediate)

---
+++ Assets/Scripts/UserInfo.cs
@@ -0,0 +1,59 @@
+using System;
+using Unity.VisualScripting;
+
+public sealed class UserInfo : IInitializable
+{
+    public UserData UserData { get; private set; }
+
+    public void Initialize()
+    {
+        UserData = new UserData();
+
+        // TODO: load saves
+    }
+}
+
+[Serializable]
+public sealed class UserData
+{
+    public event Action<int> ValueChanged;
+
+    public int Gold { get; private set; }
+
+    public UserData()
+    {
+        Gold = 250;
+    }
+
+    public void IncreaseValue(int value)
+    {
+        Gold += value;
+
+        ValueChanged?.Invoke(value);
+    }
+
+    public bool TryDecreaseValue(int value) 
+    {
+        if (Gold - value < 0)
+            return false;
+
+        Gold -= value;
+
+        ValueChanged?.Invoke(value);
+
+        return true;
+    }
+}
+
+[Serializable]
+public sealed class UserProgress
+{
+
+
+    public sealed class LevelProgress
+    {
+        public int Level { get; private set; }
+        public bool IsPassed { get; private set; }
+        public int CountStars { get; private set; }
+    }
+}
