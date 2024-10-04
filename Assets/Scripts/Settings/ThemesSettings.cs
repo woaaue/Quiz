2a68a5d720648d581eb21c41812837d55fcda1ee
woaaue <safat200@mail.ru>
2024-10-04 15:52:49 +0300

    add genering data

--- Assets/Scripts/Settings/ThemesSettings.cs
+++ Assets/Scripts/Settings/ThemesSettings.cs
@@ -1,8 +1,23 @@
 using UnityEngine;
+using NaughtyAttributes;
 using System.Collections.Generic;
 
 [CreateAssetMenu(menuName = "Quiz/ThemesSettings", fileName = "ThemesSettings", order = 1)]
 public sealed class ThemesSettings : ScriptableObject
 {
     [field: SerializeField] public List<ThemeSettings> ThemeSettings { get; private set; }
+
+#if UNITY_EDITOR
+
+    [Button("Generate ID levels and question settings")]
+    private void GenerateData()
+    {
+        ThemeSettings.ForEach(themeSettings =>
+        {
+            themeSettings.FillQuestions();
+            themeSettings.Levels.GenerateId();
+        });
+    }
+
+#endif
 }
