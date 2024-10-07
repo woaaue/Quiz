9563a7d857e00f6284988b59f53d063b934de7cb
woaaue <safat200@mail.ru>
2024-10-07 23:14:11 +0300

    add default save

--- Assets/Scripts/Storage/Base/DataSave.cs
+++ Assets/Scripts/Storage/Base/DataSave.cs
@@ -4,10 +4,17 @@ using System.Threading.Tasks;
 
 public sealed class DataSave : DataBase
 {
-    public async Task Save<T>(T data) where T : class, new()
+    public async Task SaveAsync<T>(T data) where T : class, new()
     {
         var path = GetPath<T>();
         var json = JsonUtility.ToJson(data, true);
         await File.WriteAllTextAsync(path, json);
+    }
+
+    public void Save<T>(T data) where T: class, new()
+    {
+        var path = GetPath<T>();
+        var json = JsonUtility.ToJson(data, true);
+        File.WriteAllText(path, json);
     }
 }
