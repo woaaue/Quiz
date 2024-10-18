7d59e2dd17f9c2680f81c291b0cc835a3f4c3ace
woaaue <safat200@mail.ru>
2024-10-18 12:53:00 +0300

    delete useless

--- Assets/Scripts/Systems/PopupSystem/PopupBase.cs
+++ Assets/Scripts/Systems/PopupSystem/PopupBase.cs
@@ -5,8 +5,6 @@ public class PopupBase : MonoBehaviour
 {
     [field: SerializeField] public PopupAnimator Animator { get; private set; }
 
-    public virtual bool isActiveBackground { get; private protected set; }
-
     [UsedImplicitly]
     public void ClosePopup()
     {
