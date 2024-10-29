fe41301a2723af0ce9ee15d44803996a69718fd6
woaaue <safat200@mail.ru>
2024-10-29 17:40:50 +0300

    edit purchase view

--- Assets/Scripts/Systems/ShopSystem/Purchase.cs
+++ Assets/Scripts/Systems/ShopSystem/Purchase.cs
@@ -5,34 +5,28 @@ using UnityEngine.UI;
 public sealed class Purchase : MonoBehaviour
 {
     [SerializeField] private Image _icon;
-    [SerializeField] private Image _coinIcon;
     [SerializeField] private TextMeshProUGUI _price;
     [SerializeField] private GameObject _freePurchase;
     [SerializeField] private TextMeshProUGUI _countMoney;
-    [SerializeField] private TextMeshProUGUI _description;
 
     private string _id;
 
     public void Setup(PurchaseSetting setting)
     {
-        if (setting.PurchaseType == PurchaseType.Ad)
+        _id = setting.PurchaseData.Id;
+        _icon.sprite = setting.Icon;
+        _price.text = setting.PurchaseData.Price.ToString();
+        _countMoney.text = setting.PurchaseData.Reward.Value.ToString();
+
+        if (setting.PurchaseData.PurchaseMethodType == PurchaseMethodType.WatchAd)
         {
             _price.gameObject.SetActive(false);
             _freePurchase.gameObject.SetActive(true);
         }
 
-        if (setting.RewardType == RewardType.AdRemove)
+        if (setting.PurchaseData.Reward.Type == RewardType.AdRemove)
         {
-            _coinIcon.gameObject.SetActive(false);
-            _countMoney.gameObject.SetActive(false);
-            _description.gameObject.SetActive(true);
-
-            _description.text = LocalizationProvider.GetText(LocalizationItemType.UI, setting.Id);
+            _countMoney.text = LocalizationProvider.GetText(LocalizationItemType.UI, setting.PurchaseData.Id);
         }
-
-        _id = setting.Id;
-        _icon.sprite = setting.Icon;
-        _price.text = setting.Price.ToString();     
-        _countMoney.text = setting.Value.ToString();
     }
 }
