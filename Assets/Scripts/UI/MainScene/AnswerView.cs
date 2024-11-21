1a8d0861316761f733115dfe9650288dcc23d242
woaaue <safat200@mail.ru>
2024-11-21 19:39:33 +0300

    edit answer view

--- Assets/Scripts/UI/MainScene/AnswerView.cs
+++ Assets/Scripts/UI/MainScene/AnswerView.cs
@@ -1,26 +1,37 @@
 using TMPro;
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 using JetBrains.Annotations;
 
 public sealed class AnswerView : MonoBehaviour
 {
+    [SerializeField] private Image _image;
     [SerializeField] private TextMeshProUGUI _answer;
 
-    public event Action AnimationFinished;
-    public event Action<bool> AnswerSelected;
+    public event Action<bool> Selected;
 
     private AnswerSettings _answerSettings;
 
     [UsedImplicitly]
     public void Select()
     {
-        AnswerSelected?.Invoke(_answerSettings.IsCorrect);
+        if (_answerSettings.IsCorrect)
+        {
+            _image.color = Color.green;
+        }
+        else
+        {
+            _image.color = Color.red;
+        }
+
+        Selected?.Invoke(_answerSettings.IsCorrect);
     }
 
     public void SetAnswerData(AnswerSettings settings)
     {
         _answerSettings = settings;
+        _image.color = Color.white;
         _answer.text = LocalizationProvider.GetText(LocalizationItemType.Answer, _answerSettings.Id);
     }
 }
